ba19949f5cf5e68d973b15f3261cd689dea080fb
Catnies <catnies@foxmail.com>
2026-08-07 02:44:02 +0800

    1.0.12

--- build.gradle.kts
+++ build.gradle.kts
@@ -15,7 +15,7 @@ allprojects {
     apply(plugin = "maven-publish")
 
     // 属性
-    version = "1.0.11"
+    version = "1.0.12"
     java.sourceCompatibility = JavaVersion.VERSION_17
     java.targetCompatibility = JavaVersion.VERSION_17
 
